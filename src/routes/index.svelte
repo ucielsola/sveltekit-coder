3cd85a003966deaf91c8da3ed6e980ac330a9716
Uciel Sola <solauciel@gmail.com>
2022-03-28 18:23:52 -0300

    created content for index

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Tutorial SvelteKit</h1>
+<h2>Parte 1</h2>
+<p>
+	Hola! Estás viendo la versión final de este tutorial. Es una SPA de tres secciones, creada con <a
+		href="https://kit.svelte.com"
+		title="Sitio oficial"
+		rel="noopener noreferrer"
+		target="_blank"
+		class="highlight">SvelteKit</a
+	>, un framework excelente tanto para quienes recién comienzan a aprender JavaScript, como para los
+	desarrolladores con experiencia en otros lenguajes, frameworks y librerías.
+</p>
+<p>
+	En la página <a href="/tutorial" class="highlight" title="Ir al tutorial">Tutorial</a> vas a
+	encontrar el paso-a-paso para recrear este proyecto, con toda la información necesaria y links
+	externos con datos interesantes. <br />
+	El contenido de este tutorial:
+</p>
+
+<ul>
+	<li>Creación de un proyecto SvelteKit</li>
+	<li>Estructura de un proyecto SvelteKit</li>
+	<li>Estructura de un archivo .svelte</li>
+    <li>Subir repositorio a GitHub</li>
+    <li>Publicar proyecto en Vercel (hosting gratuito)</li>
+</ul>
