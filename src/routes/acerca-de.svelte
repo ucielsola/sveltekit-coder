a1fd0746aa42e5ea93d6dbca136199defc836f2d
Uciel Sola <solauciel@gmail.com>
2022-03-28 18:23:43 -0300

    created about page

---
+++ src/routes/acerca-de.svelte
@@ -0,0 +1,20 @@
+<h1>Sobre mí</h1>
+<p>
+	Mi nombre es <span class="highlight">Uciel Sola</span>, soy tutor en los cursos de JavaScript de
+	CoderHouse, y desarrollador frontend en
+	<a
+		href="https://viset.app"
+		class="highlight"
+		title="Sitio oficial (en construcción)"
+		rel="noopener noreferrer"
+		target="_blank">Viset.App</a
+	>. Podés visitar mi web personal en
+	<a href="https://ucielsola.com" class="highlight" target="_blank">UcielSola.com</a> y contactarme
+	a través de
+	<a
+		href="https://linkedin.com/in/ucielsola"
+		class="highlight"
+		target="_blank"
+		rel="noopener noreferrer">LinkedIn</a
+	>.
+</p>
