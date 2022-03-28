827a7f589e40505fe90e37e251834796deb61446
Uciel Sola <solauciel@gmail.com>
2022-03-28 18:23:03 -0300

    created site footer

---
+++ src/lib/components/siteFooter.svelte
@@ -0,0 +1,13 @@
+<footer>
+    hecho con ⚡ por <a href="https://ucielsola.com" class="highlight" rel="noopener noreferrer" target="_blank">Uciel Sola</a>
+</footer>
+
+<style>
+    footer {
+        position: fixed;
+        bottom: 0;
+        background-color: var(--base-color-200);
+        width: 100%;
+        text-align: center;
+    }
+</style>
