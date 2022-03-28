48015e899f7b8e795934a77df8d6db0bdc5b4ef8
Uciel Sola <solauciel@gmail.com>
2022-03-28 19:26:24 -0300

    styles for codeBlock

---
+++ src/lib/components/codeBlock.svelte
@@ -0,0 +1,29 @@
+<script>
+	export let text;
+</script>
+
+<pre>
+    <code>
+        {text}
+    </code>
+</pre>
+
+<style>
+	pre {
+		display: grid;
+		margin-block: 0.5rem;
+	}
+	code {
+		display: block;
+		background: var(--base-color-200);
+		white-space: pre;
+		-webkit-overflow-scrolling: touch;
+		overflow-x: auto;
+		max-width: 100%;
+		min-width: 100px;
+		padding: 0;
+		font-family: monospace;
+		border-radius: 1rem;
+		box-shadow: var(--shadow);
+	}
+</style>
